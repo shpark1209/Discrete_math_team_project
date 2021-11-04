878d26276177d2783e27e486f249177ce2985848
joon <jsp020206@gmail.com>
2021-11-05 05:28:04 +0900

    feat interface

--- src/myPackage/GraphInterpreter.java
+++ src/myPackage/GraphInterpreter.java
@@ -1,5 +1,7 @@
 package myPackage;
 
 public interface GraphInterpreter {
-
+	// 과제에 명시된 출력 예제를 출력해주면 됩니다.
+	@Override
+	String toString();
 }
--- src/myPackage/GraphReader.java
+++ src/myPackage/GraphReader.java
@@ -1,5 +1,15 @@
 package myPackage;
 
 public interface GraphReader {
-
+	// 주어진 txt 파일을 읽어냅니다.
+	void readFile(String fileName);
+	
+	// 다음 그래프가 있는지 확인합니다.
+	boolean hasNext();
+	
+	// 다음 그래프 정보를 2차원 배열에 담아서 반환합니다. 
+	int[][] getNextData(); 
+	
+	// 인접 행렬의 경우 2차원 배열, 하지만 다익스트라 문제의 경우는?? -> 추가 가능
+	int give();
 }
